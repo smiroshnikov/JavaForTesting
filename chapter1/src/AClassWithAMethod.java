765b413babda25db5f994d17950191d809a49228
smiroshnikov <sergeim.qa@gmail.com>
2017-01-06 18:41:56 +0200

    JUnit start

---
+++ chapter1/src/AClassWithAMethod.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Iidwuurliik on 1/6/2017.
+ */
+public class AClassWithAMethod {
+    public void aMethodOnACLass() {
+        System.out.println("FFS!");
+    }
+}
---
+++ chapter1/src/AnEmptyClass.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Iidwuurliik on 1/6/2017.
+ */
+public class AnEmptyClass {
+}
